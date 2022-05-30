492a80f437c450e118969d45dd4c661cbe6d88ee
NPics <cryptoshowart@pm.me>
2022-05-30 10:22:17 +0800

    claimRewardsTo_

--- NPics.sol
+++ NPics.sol
@@ -10,17 +10,22 @@ import "./IWETHGateway.sol";
 
 contract Constants {
     //bytes32 internal constant _permissionless_  = 'permissionless';
-    //uint256 internal constant MAX_FEE_RATE      = 0.10 ether;   // 10%
+    //uint internal constant MAX_FEE_RATE      = 0.10 ether;   // 10%
 
     bytes32 internal constant _GemSwap_         = "GemSwap";
 
     address internal constant _dYdX_SoloMargin_ = 0x1E0447b19BB6EcFdAe1e4AE1694b0C3659614e4e;
     address internal constant _WETH_            = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    address internal constant _BEND_            = 0x0d02755a5700414B26FF040e1dE35D337DF56218;
     address internal constant _bendWETHGateway_ = 0x3B968D2D299B895A5Fcf3BBa7A64ad0F566e6F88;
     address internal constant _bendDebtWETH_    = 0x87ddE3A3f4b629E389ce5894c9A1F34A7eeC5648;
+    //address internal constant _bendWETH_        = 0xeD1840223484483C0cb050E6fC344d1eBF0778a9;
     //address internal constant _LendPoolAddressesProvider_ = 0x24451F47CaF13B24f4b5034e1dF6c0E401ec0e46;
-    //address internal constant _LendPool_      = 0x70b97A0da65C15dfb0FFA02aEE6FA36e507C2762;
-    address internal constant _LendPoolLoan_    = 0x5f6ac80CdB9E87f3Cfa6a90E5140B9a16A361d5C;
+    //address internal constant _bendLendPool_    = 0x70b97A0da65C15dfb0FFA02aEE6FA36e507C2762;
+    address internal constant _bendLendPoolLoan_= 0x5f6ac80CdB9E87f3Cfa6a90E5140B9a16A361d5C;
+    address internal constant _bendIncentives_  = 0x26FC1f11E612366d3367fc0cbFfF9e819da91C8d;   // BendProtocolIncentivesController
+
     address internal constant _NPics_           = 0xA2f78200746F73662ea8b5b721fDA86CB0880F15;
     address internal constant _BeaconProxyNBP_  = 0x70643f0DFbA856071D335678dF7ED332FFd6e3be;
     bytes32 internal constant _SHARD_NEO_       = 0;
@@ -66,7 +71,7 @@ contract NEO is ERC721UpgradeSafe, Constants {      // NFT Everlasting Options
         _;
     }
     
-    function transfer_(address sender, address recipient, uint256 tokenId) external onlyBeacon {
+    function transfer_(address sender, address recipient, uint tokenId) external onlyBeacon {
         _transfer(sender, recipient, tokenId);
     }
     
@@ -80,7 +85,7 @@ contract NEO is ERC721UpgradeSafe, Constants {      // NFT Everlasting Options
     }
 
     // Reserved storage space to allow for layout changes in the future.
-    uint256[48] private ______gap;
+    uint[48] private ______gap;
 }
 
 contract NBP is DydxFlashloanBase, ICallee, IERC721Receiver, ReentrancyGuardUpgradeSafe, ContextUpgradeSafe, Constants {      // NFT Backed Position
@@ -113,16 +118,23 @@ contract NBP is DydxFlashloanBase, ICallee, IERC721Receiver, ReentrancyGuardUpgr
         IERC721(nft).safeTransferFrom(address(this), to, tokenId);
     }
 
-    function downPayWithETH(TradeDetails[] memory tradeDetails, uint loanAmt) public payable nonReentrant onlyBeacon {
+    function claimRewardsTo_(address to) external onlyBeacon returns(uint amt) {
+        address[] memory assets = new address[](1);
+        assets[0] = _bendDebtWETH_;
+        amt = IBendIncentives(_bendIncentives_).claimRewards(assets, uint(-1));
+        IERC20(_BEND_).transfer(to, amt);
+    }
+
+    function downPayWithETH(TradeDetails[] memory tradeDetails, uint loanAmt) public payable onlyBeacon {
         address _solo = _dYdX_SoloMargin_;
         address _token = _WETH_;
         // Get marketId from token address
-        uint256 marketId = _getMarketIdFromTokenAddress(_solo, _token);
+        uint marketId = _getMarketIdFromTokenAddress(_solo, _token);
 
         uint _amount = IERC20(_token).balanceOf(_solo);
         // Calculate repay amount (_amount + (2 wei))
         // Approve transfer from
-        uint256 repayAmount = _amount.add(2);   //_getRepaymentAmountInternal(_amount);
+        uint repayAmount = _amount.add(2);   //_getRepaymentAmountInternal(_amount);
         IERC20(_token).approve(_solo, repayAmount);
 
         // 1. Withdraw $
@@ -154,7 +166,7 @@ contract NBP is DydxFlashloanBase, ICallee, IERC721Receiver, ReentrancyGuardUpgr
     ) override external {
         require(_msgSender() == _dYdX_SoloMargin_ && sender == address(this) && account.owner == address(this) && account.number == 1, "callFunction param check fail");
         (TradeDetails[] memory tradeDetails, uint loanAmt) = abi.decode(data, (TradeDetails[], uint));
-        uint256 balOfLoanedToken = IERC20(_WETH_).balanceOf(address(this));
+        uint balOfLoanedToken = IERC20(_WETH_).balanceOf(address(this));
         WETH9(_WETH_).withdraw(balOfLoanedToken);
         require(address(this).balance >= tradeDetails[0].value, "Insufficient downPay+flashLoan to batchBuyWithETH");
 
@@ -170,7 +182,7 @@ contract NBP is DydxFlashloanBase, ICallee, IERC721Receiver, ReentrancyGuardUpgr
         WETH9(_WETH_).deposit{value: balOfLoanedToken.add(2)}();
     }
 
-    function onERC721Received(address operator, address from, uint256 tokenId_, bytes calldata data) override external returns (bytes4) {
+    function onERC721Received(address operator, address from, uint tokenId_, bytes calldata data) override external returns (bytes4) {
         operator;
         from;
         data;
@@ -186,7 +198,7 @@ contract NBP is DydxFlashloanBase, ICallee, IERC721Receiver, ReentrancyGuardUpgr
     }
 
     // Reserved storage space to allow for layout changes in the future.
-    uint256[47] private ______gap;
+    uint[47] private ______gap;
 }
 
 contract NPics is Configurable, ReentrancyGuardUpgradeSafe, ContextUpgradeSafe, Constants {
@@ -286,41 +298,103 @@ contract NPics is Configurable, ReentrancyGuardUpgradeSafe, ContextUpgradeSafe, 
     }
     event DownPayWithETH(address indexed sender, address indexed nft, uint indexed tokenId, uint value, uint loanAmt);
 
-    function downPayBatchBuyWithETH(address nft, uint tokenId, bytes memory dataBatchBuyWithETH, uint loanAmt) external payable {
-        (bytes4 batchBuyWithETH, TradeDetails[] memory tradeDetails) = abi.decode(dataBatchBuyWithETH, (bytes4, TradeDetails[]));
-        require(batchBuyWithETH == IGemSwap.batchBuyWithETH.selector, "not batchBuyWithETH.selector");
+    function downPayBatchBuyWithETH(address nft, uint tokenId, bytes calldata data, uint loanAmt) external payable {
+        bytes4 sig = data[0] |  bytes4(data[1]) >> 8 | bytes4(data[2]) >> 16  | bytes4(data[3]) >> 24;
+        require(sig == IGemSwap.batchBuyWithETH.selector, "not batchBuyWithETH.selector");
+        TradeDetails[] memory tradeDetails = abi.decode(data[4:], (TradeDetails[]));
         downPayWithETH(nft, tokenId, tradeDetails, loanAmt);
     }
 
-    function getLoanReserveBorrowAmount(address nftAsset, uint nftTokenId) external view returns (address reserveAsset, uint256 repayDebtAmount) {
-        uint loanId = ILendPoolLoan(_LendPoolLoan_).getCollateralLoanId(nftAsset, nftTokenId);
+    function getLoanReserveBorrowAmount(address nftAsset, uint nftTokenId) public view returns(address reserveAsset, uint repayDebtAmount) {
+        uint loanId = ILendPoolLoan(_bendLendPoolLoan_).getCollateralLoanId(nftAsset, nftTokenId);
         if(loanId == 0)
             return (address(0), 0);
-        return ILendPoolLoan(_LendPoolLoan_).getLoanReserveBorrowAmount(loanId);
+        return ILendPoolLoan(_bendLendPoolLoan_).getLoanReserveBorrowAmount(loanId);
+    }
+
+    function getDebtWEthOf(address user) external view returns(uint amt) {
+        for(uint i=0; i<neoA.length; i++) {
+            NEO neo = NEO(neoA[i]);
+            address nft = neo.nft();
+            for(uint j=0; j<neo.balanceOf(user); j++) {
+                (address reserveAsset, uint repayDebtAmount) = getLoanReserveBorrowAmount(nft, neo.tokenOfOwnerByIndex(user, j));
+                if(reserveAsset == _WETH_)
+                    amt = amt.add(repayDebtAmount);
+            }
+        }
     }
     
-    function repayETH(address nftAsset, uint nftTokenId, uint amount) external payable nonReentrant returns (uint repayAmount, bool repayAll) {
+    function repayETH(address nftAsset, uint nftTokenId, uint amount) external payable nonReentrant returns(uint repayAmount, bool repayAll) {
         if(amount > 0)
             (repayAmount, repayAll) = IWETHGateway(_bendWETHGateway_).repayETH{value: msg.value}(nftAsset, nftTokenId, amount);
         if(amount == 0 || repayAll) {
             NEO neo = NEO(neos[nftAsset]);
-            NBP(nbps[nftAsset][nftTokenId]).withdraw_(neo.ownerOf(nftTokenId));
+            require(address(neo) != address(0) && address(neo).isContract(), "INVALID neo");
+            address user = neo.ownerOf(nftTokenId);
+            NBP nbp = NBP(nbps[nftAsset][nftTokenId]);
+            uint rwd = nbp.claimRewardsTo_(user);
+            emit RewardsClaimed(user, rwd);
+            nbp.withdraw_(user);
             neo.burn_(nftTokenId);
         }
-
+        if(address(this).balance > 0)
+            _msgSender().transfer(address(this).balance);
         emit RepayETH(_msgSender(), nftAsset, nftTokenId, repayAmount, repayAll);
+    }
+    event RepayETH(address indexed sender, address indexed nftAsset, uint indexed nftTokenId, uint repayAmount, bool repayAll);
 
+    function batchRepayETH(address[] calldata nftAssets, uint256[] calldata nftTokenIds, uint256[] calldata amounts) external payable nonReentrant returns(uint256[] memory repayAmounts, bool[] memory repayAlls) {
+        (repayAmounts, repayAlls) = IWETHGateway(_bendWETHGateway_).batchRepayETH{value: msg.value}(nftAssets, nftTokenIds, amounts);
+        for(uint i=0; i<repayAmounts.length; i++) {
+            if(repayAlls[i]) {
+                NEO neo = NEO(neos[nftAssets[i]]);
+                require(address(neo) != address(0) && address(neo).isContract(), "INVALID neo");
+                address user = neo.ownerOf(nftTokenIds[i]);
+                NBP nbp = NBP(nbps[nftAssets[i]][nftTokenIds[i]]);
+                uint rwd = nbp.claimRewardsTo_(user);
+                emit RewardsClaimed(user, rwd);
+                nbp.withdraw_(user);
+                neo.burn_(nftTokenIds[i]);
+            }
+            emit RepayETH(_msgSender(), nftAssets[i], nftTokenIds[i], repayAmounts[i], repayAlls[i]);
+        }
         if(address(this).balance > 0)
             _msgSender().transfer(address(this).balance);
     }
-    event RepayETH(address indexed sender, address indexed nftAsset, uint indexed nftTokenId, uint repayAmount, bool repayAll);
+
+    function getRewardsBalance(address user) external view returns(uint amt) {
+        address[] memory assets = new address[](1);
+        assets[0] = _bendDebtWETH_;
+        for(uint i=0; i<neoA.length; i++) {
+            NEO neo = NEO(neoA[i]);
+            address nft = neo.nft();
+            for(uint j=0; j<neo.balanceOf(user); j++) {
+                address nbp = nbps[nft][neo.tokenOfOwnerByIndex(user, j)];
+                amt = amt.add(IBendIncentives(_bendIncentives_).getRewardsBalance(assets, nbp));
+            }
+        }
+    }
+
+    function claimRewards() external returns(uint amt) {
+        address user = _msgSender();
+        for(uint i=0; i<neoA.length; i++) {
+            NEO neo = NEO(neoA[i]);
+            address nft = neo.nft();
+            for(uint j=0; j<neo.balanceOf(user); j++) {
+                address payable nbp = nbps[nft][neo.tokenOfOwnerByIndex(user, j)];
+                amt = amt.add(NBP(nbp).claimRewardsTo_(user));
+            }
+        }
+        emit RewardsClaimed(user, amt);
+    }
+    event RewardsClaimed(address indexed user, uint amount);
 
     receive () external payable {
         
     }
 
     // Reserved storage space to allow for layout changes in the future.
-    uint256[45] private ______gap;
+    uint[45] private ______gap;
 }
 
 
@@ -337,27 +411,31 @@ contract BeaconProxyNBP is Proxy, Constants {
 }
 
 
+interface WETH9 {
+    function deposit() external payable;
+    function withdraw(uint wad) external;
+}
+
 struct TradeDetails {
-    uint256 marketId;
-    uint256 value;
+    uint marketId;
+    uint value;
     bytes tradeData;
-}
-
-interface IDebtToken {
-    function approveDelegation(address delegatee, uint256 amount) external;
-}
-
-interface ILendPoolLoan {
-    function getCollateralLoanId(address nftAsset, uint256 nftTokenId) external view returns (uint256);
-    function getLoanReserveBorrowAmount(uint256 loanId) external view returns (address, uint256);
 }
 
 interface IGemSwap {
     function batchBuyWithETH(TradeDetails[] memory tradeDetails) payable external;
 }
 
-interface WETH9 {
-    function deposit() external payable;
-    function withdraw(uint wad) external;
+interface IDebtToken {
+    function approveDelegation(address delegatee, uint amount) external;
 }
 
+interface ILendPoolLoan {
+    function getCollateralLoanId(address nftAsset, uint nftTokenId) external view returns(uint);
+    function getLoanReserveBorrowAmount(uint loanId) external view returns(address, uint);
+}
+
+interface IBendIncentives {
+    function getRewardsBalance(address[] calldata assets, address user) external view returns(uint);
+    function claimRewards(address[] calldata assets, uint amount) external returns(uint);
+}
